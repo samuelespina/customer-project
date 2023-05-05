e3f7e80adb5d3cbfa427cd766413fcd7329d8cbd
samuelespina <samuelespina360@gmail.com>
2023-05-05 23:52:32 +0200

    update

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -7,7 +7,6 @@ const Navbar = () => {
   useEffect(() => {
     window.addEventListener("scroll", () => {
       setScrollValue(window.pageYOffset);
-      console.log(window.pageYOffset);
     });
   }, []);
 
